93fdd9f93ee0c6438072a5a661077743f3b39136
Grigoriy Sukhorukov <grigoriy.sukhorukov@soft-machine.ru>
2021-08-20 14:47:07 +0300

    Fix result builder

--- Source/ResultBuilder.swift
+++ Source/ResultBuilder.swift
@@ -10,12 +10,16 @@ import Foundation
 
 @resultBuilder
 public struct ResultBuilder<Value> {
-    public static func buildBlock(_ settings: Value...) -> [Value] {
-        settings
+    public static func buildBlock(_ components: [Value]...) -> [Value] {
+        components.flatMap({ $0 })
     }
     
-    public static func buildIf(_ value: Value?) -> [Value] {
-        value != nil ? [value!] : []
+    public static func buildOptional(_ component: [Value]?) -> [Value] {
+        component ?? []
+    }
+    
+    public static func buildExpression(_ component: Value) -> [Value] {
+      return [component]
     }
     
     public static func buildEither(first: [Value]) -> [Value] {
@@ -24,10 +28,6 @@ public struct ResultBuilder<Value> {
 
     public static func buildEither(second: [Value]) -> [Value] {
         second
-    }
-    
-    public static func buildBlock(_ components: [Value]...) -> [Value] {
-        return components.flatMap({ $0 })
     }
     
     public static func buildArray(_ components: [[Value]]) -> [Value] {
